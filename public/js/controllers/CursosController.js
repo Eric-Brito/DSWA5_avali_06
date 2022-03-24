4da72e19cf091f95158271dbf2de42f5052a8f91
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-24 19:36:21 -0300

    Update CursosController.js

--- public/js/controllers/CursosController.js
+++ public/js/controllers/CursosController.js
@@ -2,6 +2,7 @@ angular.module('ifsp').controller('CursosController',
     function($resource, $scope) {
         $scope.cursos = [];
         $scope.filtro = '';
+        $scope.mensagem = { texto: '' };
         var Curso = $resource('/cursos');
 
         function buscaCursos() {
@@ -16,4 +17,14 @@ angular.module('ifsp').controller('CursosController',
             );
         }
         buscaCursos();
+        $scope.remove = function(curso) {
+            console.log(curso);
+            Contato.delete({ id: curso._id },
+                           buscaCursos,
+                           function(erro) {
+                                console.log("Não foi possível remover o curso"};
+                                console.log(erro);
+                                $scope.mensagem = { texto: "Não foi possível remover o curso" };
+                            });
+        };      
     });
