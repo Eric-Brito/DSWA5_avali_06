b86f843c0c0737d5a5ce3aad4b7e94d92667070c
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-03-24 19:30:41 -0300

    Update ContatosController.js

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -21,7 +21,7 @@ angular.module('ifsp').controller('ContatosController',
         buscaContatos();
         $scope.remove = function(contato) {
             console.log(contato);
-            Contato.delete({ id: contato. id },
+            Contato.delete({ id: contato._id },
                            buscaContatos,
                            function(erro) {
                                 console.log("Não foi possível remover o contato"};
